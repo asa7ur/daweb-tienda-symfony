c688229177c3a029339cee754d9f117a60cac1c8
Garik Asatryan <asa7ur@proton.me>
2025-11-18 20:00:54 +0100

    Añadidos productos y categorias

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -7,14 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Categoria;
-use App\Entity\Producto;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 // Clase controladora para ROLE_USER
 #[IsGranted('ROLE_USER')]
 final class BaseController extends AbstractController
 {
-    #[Route('/categorias', name: 'app_categorias')]
+    #[Route('/categorias', name: 'categorias')]
     public function mostrar_categorias(EntityManagerInterface $em): Response
     {
         $categorias = $em->getRepository(Categoria::class)->findAll();
@@ -24,9 +23,10 @@ final class BaseController extends AbstractController
         ]);
     }
 
-    #[Route('/productos/{categoria}', name: 'app_productos', defaults: ['categoria' => null])]
+    #[Route('/productos/{categoria}', name: 'productos', defaults: ['categoria' => null])]
     public function mostrar_productos(EntityManagerInterface $em, ?int $categoria = null): Response
-    {        // Obtenemos el objeto categoria correspondiente al id pasado como parametro
+    {        
+        // Obtenemos el objeto categoria correspondiente al id pasado como parametro
         $categoriaObj = $em->getRepository(Categoria::class)->find($categoria);
         if (!$categoriaObj) {
             throw $this->createNotFoundException('Categoría no encontrada');
@@ -38,18 +38,40 @@ final class BaseController extends AbstractController
             'productos' => $productos,
         ]);
     }
-
-    #[Route('/producto/{id}', name: 'app_producto')]
-    public function mostrar_producto(EntityManagerInterface $em, int $id): Response
-    {
+    
+    #[Route('/anadir', name: 'anadir')]
+    public function anadir_producto(
+        int $id,
+        EntityManagerInterface $em,
+        Request $request
+    ): RedirectResponse {
         $producto = $em->getRepository(Producto::class)->find($id);
 
-        if (!$producto) {
+        /* if (!$producto) {
             throw $this->createNotFoundException('Producto no encontrado');
         }
 
-        return $this->render('producto/mostrar_producto.html.twig', [
-            'producto' => $producto,
-        ]);
+        // Obtenemos la cesta actual desde la sesión
+        $session = $request->getSession();
+        $cesta = $session->get('cesta', []);
+
+        // Añadimos el producto o incrementamos cantidad si ya existe
+        if (isset($cesta[$id])) {
+            $cesta[$id]['cantidad']++;
+        } else {
+            $cesta[$id] = [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'precio' => $producto->getPrecio(),
+                'cantidad' => 1,
+            ];
+        }
+
+        // Guardamos de nuevo en sesión
+        $session->set('cesta', $cesta);
+
+        // Redirigimos a la ruta 'cesta'
+        return $this->redirectToRoute('cesta');
+        */
     }
 }
